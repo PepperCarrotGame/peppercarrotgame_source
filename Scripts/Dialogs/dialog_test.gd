13d596124d0c646df62ea2b4bbda144766faaf4c
Fran Paredes <franciscoluis.paredes@gmail.com>
2016-03-25 11:18:21 +0100

    Dialogs system testing

---
+++ Scripts/Dialogs/dialog_test.gd
@@ -0,0 +1,29 @@
+# ==== Pepper & Carrot Game ====
+#
+# Purpose: Display the character's dialog text
+#
+# ==============================
+
+extends Label
+
+# Text speed y chars per second
+export var speed = 10
+export var character_name = "Type Name here"
+export var animated_text = "Type text here"
+var counter = 0;
+
+func _ready():
+	set_process(true)
+	get_node("../Character Name").set_text(str(character_name, ":"))
+
+func _process(delta):
+	animate_text(delta)
+	
+
+func animate_text(delta):
+	counter += delta
+	var characters = int(counter*speed)
+	
+	set_text(animated_text.substr(0, characters));
+	if (characters > animated_text.length()):
+		set_process(false)
